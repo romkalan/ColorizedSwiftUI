be8b9dd3c4c57a2eb4454cb2183e9a97adee8174
romkalan <romkalan@mail.ru>
2023-04-24 12:09:14 +0300

    add sliders manipulation for setup color

--- ColorizedSwiftUI/ContentView.swift
+++ ColorizedSwiftUI/ContentView.swift
@@ -8,14 +8,64 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var redSliderValue = Double.random(in: 0...255)
+    @State private var greenSliderValue = Double.random(in: 0...255)
+    @State private var blueSliderValue = Double.random(in: 0...255)
+    @State private var redSliderTF = ""
+    @State private var greenSliderTF = ""
+    @State private var blueSliderTF = ""
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        ZStack {
+            Color.black.ignoresSafeArea()
+            VStack(spacing: 10) {
+                Color(
+                    red: convertValue(from: redSliderValue),
+                    green: convertValue(from: greenSliderValue),
+                    blue: convertValue(from: blueSliderValue)
+                )
+                    .frame(height: 150)
+                    .clipShape(RoundedRectangle(cornerRadius: 20))
+                    .overlay(RoundedRectangle(cornerRadius: 20).stroke(Color.white, lineWidth: 5))
+                    .padding(.bottom, 40)
+                HStack {
+                    Text("\(lround(redSliderValue))")
+                        .frame(width: 35)
+                        .foregroundColor(.white)
+                    Slider(value: $redSliderValue, in: 0...255, step: 1)
+                        .accentColor(.red)
+                    TextField("\(lround(redSliderValue))", text: $redSliderTF)
+                        .textFieldStyle(.roundedBorder)
+                        .frame(width: 50)
+                }
+                HStack {
+                    Text("\(lround(greenSliderValue))")
+                        .frame(width: 35)
+                        .foregroundColor(.white)
+                    Slider(value: $greenSliderValue, in: 0...255, step: 1)
+                        .accentColor(.green)
+                    TextField("\(lround(greenSliderValue))", text: $greenSliderTF)
+                        .textFieldStyle(.roundedBorder)
+                        .frame(width: 50)
+                }
+                HStack {
+                    Text("\(lround(blueSliderValue))")
+                        .frame(width: 35)
+                        .foregroundColor(.white)
+                    Slider(value: $blueSliderValue, in: 0...255, step: 1)
+                        .accentColor(.blue)
+                    TextField("\(lround(blueSliderValue))", text: $blueSliderTF)
+                        .textFieldStyle(.roundedBorder)
+                        .frame(width: 50)
+                }
+                Spacer()
+            }
+            .padding()
         }
-        .padding()
+    }
+    
+    func convertValue(from value: Double) -> Double {
+        value / 255
     }
 }
 
